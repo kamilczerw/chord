56d1dd418d5811cb7566c475460d1e22a6c6bdde
Kamil Czerwiński <kamil@czerwinski.dev>
2023-03-17 22:36:38 +0100

    Update packages

--- libs/grpc/src/server.rs
+++ libs/grpc/src/server.rs
@@ -1,24 +1,35 @@
-use tonic::{Request, Response, Status};
+use chord_proto::chord_node_server::ChordNode;
+pub use chord_proto::chord_node_server::ChordNodeServer;
+use chord_proto::{PingRequest, PingResponse};
 pub use tonic::transport::Server;
-use chord::chord_node_server::ChordNode;
-use chord::{PingRequest, PingResponse};
+use tonic::{Request, Response, Status};
 
-pub mod chord {
+use self::chord_proto::{FindSuccessorRequest, FindSuccessorResponse};
+
+pub mod chord_proto {
     include!(concat!(env!("OUT_DIR"), "/chord.rs"));
 }
 
 #[derive(Debug, Default)]
-pub struct MyChord {}
+pub struct ChordService {}
 
 #[tonic::async_trait]
-impl ChordNode for MyChord {
-    async fn ping(
-        &self,
-        request: Request<PingRequest>,
-    ) -> Result<Response<PingResponse>, Status> {
+impl ChordNode for ChordService {
+    async fn ping(&self, request: Request<PingRequest>) -> Result<Response<PingResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = chord::PingResponse {};
+        let reply = chord_proto::PingResponse {};
+
+        Ok(Response::new(reply))
+    }
+
+    async fn find_successor(
+        &self,
+        request: Request<FindSuccessorRequest>,
+    ) -> Result<Response<FindSuccessorResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = FindSuccessorResponse { id: 1, node: None };
 
         Ok(Response::new(reply))
     }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,9 @@
-use tamto_grpc::server::chord::chord_node_server::ChordNodeServer;
-use tamto_grpc::server::{MyChord, Server};
+use tamto_grpc::server::{ChordNodeServer, ChordService, Server};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let chord = MyChord::default();
+    let addr = "[::1]:50052".parse()?;
+    let chord = ChordService::default();
 
     Server::builder()
         .add_service(ChordNodeServer::new(chord))
